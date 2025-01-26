ddd38cb4d13a6c724f819bf2a7f5579b616c2593
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-01-26 21:23:22 +0900

    Update A／B.cc

--- 백준/Bronze/1008. A／B/A／B.cc
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main() {
-	long double a, b;				//변수 타입 설정
+	long double a, b;		//변수 타입 설정
 
 	cin >> a;			//변수 스캔 
 	cin >> b;
 
-	cout.precision(15);
+	cout.precision(15);		//소수점 15번째까지 출력
 	cout << a / b << endl;		//변수 출력
 
 	return 0;
