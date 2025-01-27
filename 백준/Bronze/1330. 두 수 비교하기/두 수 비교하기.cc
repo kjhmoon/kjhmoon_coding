40e713910fb94a069f1d80371a71292281bb0480
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-01-27 17:33:38 +0900

    Update 두 수 비교하기.cc

--- 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.cc
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-	int a, b;
-	cin >> a;
-	cin >> b;
+int main() {	
+	int a, b;				//변수타입 지정
+	cin >> a;				//변수 a 입력 받기
+	cin >> b;				//변수 b 입력 받기
 	
-	if (a < b) 					// 만약 a>b라면
+	if (a < b) 				// 만약 a>b라면
 		cout << "<";			// "<"출력
 
-	else if (a>b)  							// 만약 a<b라면
+	else if (a>b)  				// 아니면, 만약 a<b라면
 		cout << ">";			// ">"출력
 	
-	if (a == b) 					// a=b라면
+	if (a == b) 				// a=b라면
 		cout << "==";			// "=="출력
 	
 	return 0;
