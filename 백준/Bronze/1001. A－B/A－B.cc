eea5f343d5a3aaac1d68abaf78a1f9599a713d51
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-01-27 17:21:33 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;				//변수설정
+
+	cin >> a;			//	변수 a 받기
+	cin >> b;			// 변수 b 받기 
+
+	cout << a - b;	// a-b 출력하기 
+
+	return 0;
+}
