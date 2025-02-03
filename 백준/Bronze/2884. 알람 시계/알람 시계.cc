c611458c7d5bedd0d9e73b8add86e19e31059a94
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-03 22:36:39 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int H;
+	int M;
+
+	cin >> H >> M;
+
+	if (M < 45) {
+		H--;
+		M = 60 - (45 - M);
+		if (H < 0) {
+			H = 23;
+		}
+	}
+	else{
+		M = (M - 45);
+	}
+	cout << H << " " << M;
+	return 0;
+}
