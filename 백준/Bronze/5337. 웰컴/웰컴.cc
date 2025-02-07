94554fcf3641ca29196272cb492e5aa3b80b4e6d
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-07 22:14:24 +0900

    [Bronze V] Title: 웰컴, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+	cout << ".  .   ." << '\n';
+	cout << "|  | _ | _. _ ._ _  _" << '\n';
+	cout << "|/\\|(/.|(_.(_)[ | )(/." << '\n';
+	return 0;
+}
