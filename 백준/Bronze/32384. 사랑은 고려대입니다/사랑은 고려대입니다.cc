b615ca6ae9c44ba50098c7955c58b9d2f510ec72
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-08 10:45:31 +0900

    [Bronze V] Title: 사랑은 고려대입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32384. 사랑은 고려대입니다/사랑은 고려대입니다.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+using namespace std;
+
+int main() {
+	int N;
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		cout << "LoveisKoreaUniversity" << ' ';
+	}
+	return 0;
+}
