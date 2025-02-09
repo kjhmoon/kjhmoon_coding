16d24d56ea247dd91ca9a4eec66def3baae6af26
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-09 22:23:13 +0900

    [Bronze V] Title: 홍익대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    int a,n;
+    cin >> a;
+    n = a - 1946;
+    cout << n;
+
+    return 0;
+}
