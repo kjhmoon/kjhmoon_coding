9b0be8cd44efd34c6e787eedf77b8eeb733d1f89
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-11 20:22:00 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int a;
+    cin >> a;
+    cout << a - 543;
+    return 0;
+}
