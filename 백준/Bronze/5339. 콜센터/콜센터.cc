aca302babe80f252add2e274c871fd2696dcaf6f
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-15 15:32:29 +0900

    [Bronze V] Title: 콜센터, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+	cout << "     /~\\" << '\n';
+	cout << "    ( oo|" << '\n';
+	cout << "    _\\=/_" << '\n';
+	cout << "   /  _  \\" << '\n';
+	cout << "  //|/.\\|\\\\" << '\n';
+	cout << " ||  \\ /  ||" << '\n';
+	cout << "============" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	cout << "|          |" << '\n';
+	return 0;
+}
