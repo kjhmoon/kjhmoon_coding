6c8394a929a38fe26bd25217bc1376c7b39d0934
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-15 20:29:57 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n;
+    cin >> n;
+    n = n + 1;
+    cout << n * 2 << ' ' << n * 3;
+
+    return 0;
+}
