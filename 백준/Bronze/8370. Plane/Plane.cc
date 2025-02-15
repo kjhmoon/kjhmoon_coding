a1c6f695cd8f9277b56701f8eee2bacf8d1ab396
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-15 20:09:04 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n1, n2;
+	int n3, n4;
+	cin >> n1 >> n2 >> n3 >> n4;
+	cout << n1 * n2 + n3 * n4;
+	return 0;
+}
