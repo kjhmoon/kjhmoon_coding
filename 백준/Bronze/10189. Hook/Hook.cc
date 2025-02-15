2bdab03072517aea370c95bc6b1c67a06dd989c3
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-15 21:01:55 +0900

    [Bronze V] Title: Hook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10189. Hook/Hook.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    cout <<"#  # #### #### #  #"<< '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#### #  # #  # # #" << '\n';
+    cout << "#  # #### #### #  #"; 
+    return 0;
+}
