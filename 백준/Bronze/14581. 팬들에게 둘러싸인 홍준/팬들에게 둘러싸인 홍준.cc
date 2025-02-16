520fcd097691bc55756653a35c8ace10069273c9
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-16 22:00:19 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+    string n;
+    cin >> n;
+    cout << ":fan::fan::fan:" << '\n';
+    cout << ":fan::" << n << "::fan:" << '\n';
+    cout << ":fan::fan::fan:";
+    return 0;
+}
