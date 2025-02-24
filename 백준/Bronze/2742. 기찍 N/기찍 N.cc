5171da9755a6677bb587fdef667e574819e42988
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-24 22:21:15 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	for (int i = n; i > 0; i--){
+		cout << i << '\n';
+	}
+	return 0;
+}
