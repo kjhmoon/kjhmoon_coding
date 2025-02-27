0a584661e8ad8323990948613a9d00b82166070c
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-02-27 15:57:40 +0900

    [Bronze III] Title: 나는 요리사다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2953. 나는 요리사다/나는 요리사다.cc
+++ 백준/Bronze/2953. 나는 요리사다/나는 요리사다.cc
@@ -1,30 +1,28 @@
 #include <iostream>
-#include <string>
-#include <stdlib.h>
-#include <cmath>
-#include <algorithm>
-#include <vector>
-#include <iomanip>
 using namespace std;
+
 int main() {
-	int a, b, c, d; // 각 분야 점수
-	int sum1 = 0; //분야 점수의 합
-	int max = -1; //최대 점수
-	int count = 0; //우승자 번호
-	for (int i = 1; i <=5;i++) {
-		cin >> a >> b >> c >> d;
-		sum1 = a + b + c + d;
-		if (sum1 > max) {
-			max = sum1;
-			count = i;
-		}
-		else {
-			a = 0;
-			b = 0;
-			c = 0;
-			d = 0;
-		}
-	}
-	cout << count << ' ' << max;
-	return 0;
+    int arr[5] = { 0 };
+    int max = 0;
+    int winner = 0;
+
+    // 입력 및 총점 계산
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 4; j++) {
+            int score;
+            cin >> score;
+            arr[i] += score;
+        }
+    }
+
+    // 최고 점수 찾기
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+            winner = i + 1;
+        }
+    }
+
+    cout << winner << " " << max;
+    return 0;
 }
