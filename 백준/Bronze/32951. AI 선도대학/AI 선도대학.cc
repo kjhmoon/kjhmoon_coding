7a00291df526b6d0df13d3a663442f0f12e16500
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-03-03 20:37:48 +0900

    [Bronze V] Title: AI 선도대학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+int main() {
+	int A;
+	cin >> A;
+	cout << A - 2024;
+	return 0;
+}
