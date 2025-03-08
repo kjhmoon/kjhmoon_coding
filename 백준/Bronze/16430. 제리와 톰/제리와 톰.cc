46c256b6c31fb1482174e1347a27ca26105d3525
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-03-08 22:05:12 +0900

    [Bronze V] Title: 제리와 톰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/16430. 제리와 톰/제리와 톰.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <regex>
+using namespace std;
+
+int main() {
+    int a, b;
+    int c;
+    cin >> a >> b;
+    c = b - a;
+    cout << c << ' ' << b;
+    return 0;
+}
