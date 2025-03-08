7cb13587c1301443856f5de4d6cca870bb16ac6f
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-03-08 21:58:54 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <regex>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    if (N == 1) {
+        cout << "Leading the Way to the Future";
+    }
+    if (N == 0) {
+        cout << "YONSEI";
+    }
+    return 0;
+}
