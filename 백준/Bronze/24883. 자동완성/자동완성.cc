d936e6b9cfff96d3b9340faed485356c19f9598d
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-03-17 19:49:06 +0900

    [Bronze V] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    char a;
+    cin >> a;
+    if(a == 'n'||a == 'N'){
+        cout << "Naver D2";
+        exit(0);
+    }
+    else{
+        cout << "Naver Whale";
+}
+    return 0;
+}
