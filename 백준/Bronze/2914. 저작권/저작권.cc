550679229acca29b94b1ffeb4b9e1ad2bdd203fb
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-04-15 18:22:11 +0900

    [Bronze III] Title: 저작권, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2914. 저작권/저작권.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+using namespace std;
+
+int main() {				
+	int a, b;
+	int c;
+	cin >> a >> b;
+	c = a * (b - 1);
+	cout << c + 1;
+
+	return 0;
+}
