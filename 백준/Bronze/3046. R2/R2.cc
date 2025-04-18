1e83184af51fbfd4853b1c0812e6b528df1018d3
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-04-19 00:53:53 +0900

    [Bronze IV] Title: R2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+using namespace std;
+
+int main() {
+	int R1, S;
+	int R2;
+	cin >> R1 >> S;
+	R2 = 2 * S - R1;
+	cout << R2;
+	return 0;
+}
