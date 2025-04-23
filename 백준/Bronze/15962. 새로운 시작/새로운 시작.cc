a372aa8fad963b7272dd6f2d9befde69dd87e782
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-04-23 14:29:14 +0900

    [Bronze V] Title: 새로운 시작, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15962. 새로운 시작/새로운 시작.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+using namespace std;
+
+int main() {	
+	cout << "파이팅!!";
+	return 0;
+}
