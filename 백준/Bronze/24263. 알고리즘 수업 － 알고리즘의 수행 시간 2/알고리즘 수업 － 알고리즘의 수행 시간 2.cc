da65e5c975cde9c3f892235e05a318d4db6bcf0b
공지혁 <163113764+kjhmoon@users.noreply.github.com>
2025-08-29 13:34:29 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <utility>
+using namespace std;
+
+int main() {
+     int degree = 1;
+    int n;
+    cin >> n;
+    cout << n<< '\n' << degree;
+
+    return 0;
+}
